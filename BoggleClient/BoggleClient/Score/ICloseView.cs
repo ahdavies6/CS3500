9a70b730d5db6af3fc9c93d2c99ace9988d7d622
Nithin Chalapathi <nithin.ch10@gmail.com>
2018-03-13 20:13:33 -0600

    Add ICloseView - Marks the creation of the interfaces

---
+++ BoggleClient/BoggleClient/Score/ICloseView.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoggleClient.Score
+{
+    interface ICloseView
+    {
+        /// <summary>
+        /// Restarts the application and puts it back into the open screen
+        /// </summary>
+        // TODO: Fix the action to the proper parameters / delegate type
+        event Action CancelPushed;
+
+        /// <summary>
+        /// Extra event that we could use for making a new game 
+        /// 
+        /// NOTE: We would have to change action to the proper delegate type
+        /// </summary>
+        event Action NewGame;
+    }
+}
