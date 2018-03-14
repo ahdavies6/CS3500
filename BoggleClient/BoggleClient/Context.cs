12a7f37fd44e5fcff2a63d6501dc20d08ebc3a52
Adam Davies <ahdavies6@gmail.com>
2018-03-13 18:53:43 -0600

    temp commit

---
+++ BoggleClient/BoggleClient/Context.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BoggleClient
+{
+    class Context : ApplicationContext
+    {
+        // Singleton pattern
+        private static Context context;
+
+        private Context() { }
+
+        public static Context GetContext()
+        {
+            if (context == null)
+            {
+                context = new Context();
+            }
+            return context;
+        }
+
+        public void Run()
+        {
+            // Open Game
+        }
+    }
+}
